60d439b49f205cc3732d019d6221a67e77696894
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-04-12 17:09:59 -0400

    Add jest-mongodb dependency

---
+++ jest-mongodb-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mongodbMemoryServerOptions: {
+      binary: {
+        version: '6.0.4',
+        skipMD5: true
+      },
+      instance: {},
+      autoStart: false
+    }
+  };
+  
