3cf78cc277788dbf22fd32dc490a83dbaff108df
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-04-30 01:46:28 -0400

    Add interface & improved response for  errorHandler middleware

--- src/middleware/errorhandler.middleware.ts
+++ src/middleware/errorhandler.middleware.ts
@@ -1,9 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 
-export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+export interface CustomError extends Error {
+  statusCode?: number;
+}
+
+export function errorHandler(
+  err: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const statusCode = err.statusCode || 500;
   res.status(statusCode).json({
+    success: false,
     message: err.message || 'Internal Server Error',
+    stack: process.env.NODE_ENV === 'production' ? undefined : err.stack,
   });
-  next();
+  next
 }
