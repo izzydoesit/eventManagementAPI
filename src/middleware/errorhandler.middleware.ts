c6e178de7df6c57e1ccecac6d530eff7c6e7c64e
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-04-30 01:14:08 -0400

    Add errorhandler middleware

---
+++ src/middleware/errorhandler.middleware.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
+
+export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    message: err.message || 'Internal Server Error',
+  });
+}
