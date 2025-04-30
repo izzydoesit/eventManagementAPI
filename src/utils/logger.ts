3e45926ec35a52681c2bae77edb203407bc87880
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-04-30 12:39:29 -0400

    Improve logger, adding modularity

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,73 @@
-import winston from 'winston';
+import {createLogger, loggers, format, transports } from 'winston';
+const { combine, json, timestamp, label, prettyPrint, printf, colorize } = format;
 
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.Console({ format: winston.format.simple() }),
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'logs/combined.log' }),
-    ],
+const customFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+loggers.add('controllers', {
+  format: combine(
+    label({ label: 'CONTROLLERS' }),
+    json()
+  ),
+  transports: [
+    new transports.Console({ level: 'info' }),
+    new transports.File({ filename: 'logs/controllers.log' })
+  ]
+});
+
+loggers.add('services', {
+  format: combine(
+    label({ label: 'SERVICES' }),
+    json()
+  ),
+  transports: [
+    new transports.Console({ level: 'info' }),
+    new transports.File({ filename: 'logs/services.log' })
+  ]
+});
+
+loggers.add('routes', {
+  format: combine(
+    label({ label: 'ROUTES' }),
+    json()
+  ),
+  transports: [
+    new transports.Console({ level: 'info' }),
+    new transports.File({ filename: 'logs/routes.log' })
+  ]
+});
+
+loggers.add('Middleware', {
+  format: combine(
+    label({ label: 'MIDDLEWARE' }),
+    json()
+  ),
+  transports: [
+    new transports.Console({ level: 'info' }),
+    new transports.File({ filename: 'logs/middleware.log' })
+  ]
+});
+
+loggers.add('utilities', {
+  format: combine(
+    label({ label: 'UTILITIES' }),
+    json()
+  ),
+  transports: [
+    new transports.Console({ level: 'info' }),
+    new transports.File({ filename: 'logs/utilities.log' })
+  ]
+});
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(json(), colorize({ all: true }), timestamp(), prettyPrint(), customFormat),
+  transports: [
+    new transports.Console({ format: format.simple() }),
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/combined.log' }),
+  ],
 });
 
 export default logger;
